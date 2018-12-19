ad2c86dc84f589399a4af45eef6116399bfcf599
matt.inteltech <matt.inteltech@gmail.com>
2018-12-19 15:07:19 +0000

    add beforeClass method to App Test

--- src/test/java/com/bank/App.java
+++ src/test/java/com/bank/App.java
@@ -2,13 +2,21 @@ package com.bank;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class App {
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("I'm beforeClass method!");
+    }
+
     @Before
     public void before() {
         System.out.println("I'm before method!");
     }
+
     @Test
     public void main() {
 
@@ -20,4 +28,6 @@ public class App {
     public void after() {
         System.out.println("I'm after method!");
     }
+
+
 }
